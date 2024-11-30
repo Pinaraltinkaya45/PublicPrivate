8a18281ae4868a8a4ae27cad4752c6b99a84550d
Pinaraltinkaya45 <189837954+Pinaraltinkaya45@users.noreply.github.com>
2024-11-30 17:25:14 +0300

    Add project files.

---
+++ PublicPrivate/Müsteri.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusteriBilgi
+{
+    public class Müsteri
+    {
+        public string isim;
+        public string soyisim;
+        public int maasDegeri;
+        public string cinsiyet;
+        private int yas;
+
+        public Müsteri()
+        {
+            Console.WriteLine("Müsteri Sınıfının Yapıcı Metotu Çalıştı"); 
+        }
+        public void musteriBilgileri()
+        {
+            Console.WriteLine("İsim: " + isim);
+            Console.WriteLine("Soyisim: " + soyisim);
+            Console.WriteLine("Maaş Değeri : " + maasDegeri);
+            Console.WriteLine("Cinsiyet: " + cinsiyet);
+           // Console.WriteLine("Yas Degeri: " + yas);
+        }
+
+        private void IsimSoyisimYazdir(string musteriismi,string musterisoyismi)
+        {
+            Console.WriteLine("Müşterinin ismi ve soyismi : " + musteriismi + " " + musterisoyismi);
+
+        }
+    }
+}
---
+++ PublicPrivate/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusteriBilgi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // public: her yerden erişilir
+            // private : sadece tanımlandığı sınıf tarafından erişilir
+            Müsteri musteri1 = new Müsteri();
+
+            musteri1.isim = "Pınar";
+            musteri1.soyisim = "Altınkaya";
+            musteri1.maasDegeri = 25000;
+            musteri1.cinsiyet = "Kadın";
+
+            musteri1.musteriBilgileri();
+            
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
